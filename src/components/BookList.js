728237ffe35a906c76e8bc03918fc1633702f30b
Amy Gori <amydgori@gmail.com>
2022-04-08 20:37:26 -0400

    Add loading skeleton

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+import { Navigate } from 'react-router-dom'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import { BookCard } from './BookCard'
 export const BookList = ({ token }) => {
   const [books, setBooks] = useState([])
   const [bookTitles, setBookTitles] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     axios
@@ -16,8 +20,24 @@ export const BookList = ({ token }) => {
         const bookTitles = res.data.map((obj) => obj.title)
         setBookTitles(bookTitles)
         setBooks(res.data)
+        setIsLoading(false)
       })
   }, [token])
+
+  if (!token) {
+    return <Navigate to="/login" />
+  }
+
+  if (isLoading) {
+    return (
+      <Skeleton
+        count={10}
+        height="50px"
+        width="90%"
+        containerClassName="skeleton-container"
+      />
+    )
+  }
 
   return (
     <div className="book-list container-box">
